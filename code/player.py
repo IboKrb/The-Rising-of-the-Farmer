c0f1a7b549c2584f6b75cdf8db632dab8022bfaa
Ibrahim Aldemir <ibrahimaldemir2014@gmail.com>
2022-04-22 01:28:33 +0200

    add classes and sprites
    add player and stone class and png

---
+++ code/player.py
@@ -0,0 +1,8 @@
+import pygame
+from Settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self,pos,groups):
+        super().__init__(groups)
+        self.image = pygame.image.load("./graphics/Player.png").convert_alpha()
+        self.rect = self.image.get_rect(topleft = pos)
