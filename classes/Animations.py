cb4861fbf65f1bc023c711853c6ba64133bb4034
Ibrahim Aldemir <ibrahimaldemir2014@gmail.com>
2022-04-22 01:29:00 +0200

    delete class
    delete animations and mobs

--- classes/Animations.py
+++
@@ -1,7 +0,0 @@
-import pygame
-import os
-
-class Animations():
-    def __init__(self):
-        path_file = os.path.dirname(os.path.abspath(__file__))
-        path_image = os.path.join(path_file, "images")
