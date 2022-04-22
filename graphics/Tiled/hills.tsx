71082bb23e23fc65079211bd7ea1fc6c6f6c4f50
Ibrahim Aldemir <ibrahimaldemir2014@gmail.com>
2022-04-22 21:41:16 +0200

    fixs

--- graphics/Tiled/hills.tsx
+++ graphics/Tiled/hills.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.4" name="hills" tilewidth="16" tileheight="16" tilecount="36" columns="6">
- <image source="Sprout Lands - Sprites - Basic pack/Tilesets/Hills.png" width="96" height="96"/>
+ <image source="Tilesets/Hills.png" width="96" height="96"/>
 </tileset>
